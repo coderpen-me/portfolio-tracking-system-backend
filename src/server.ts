d0069e66a5fe6d07793ca7bb62c855a0891315a3
Adarsh Pandey <coderpen@gmail.com>
2021-02-02 21:22:57 +0530

    mod

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import app from './app';
  * port configuration and server startup
  */
 const server = app.listen(app.get('port'), async () => {
-    console.log('Server Started !!');
+    console.log('Server Started on', app.get('port'));
 });
 
 export default server;
