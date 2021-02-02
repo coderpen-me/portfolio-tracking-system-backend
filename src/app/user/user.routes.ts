b6875e2803a971b1e810467eb631fe23cea198bc
Adarsh Pandey <coderpen@gmail.com>
2021-02-03 00:30:57 +0530

    fix: STP-04 fixed swapped routes in user router

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -5,7 +5,7 @@ import { addNewUser, getUserByEmail, getUsers } from './user.controller';
 import { createUserValidationRules } from './user.validate';
 
 router.post('/', createUserValidationRules(), validate, addNewUser);
-router.get('/', getUsers);
-router.get('/all', getUserByEmail);
+router.get('/all', getUsers);
+router.get('/', getUserByEmail);
 
 export { router as userRouter };
